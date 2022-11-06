0db2be2f9c1cb2cfdefb17fc162af72ebd121af8
pierre <pierre@dahive.fr>
2022-11-06 21:23:04 +0100

    Add component Nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,23 @@
 import { FunctionComponent } from 'react';
 
+import SlideUpButton from '../SlideUpButton';
+
 import styles from './Nav.module.css';
 
 const Nav: FunctionComponent = () => {
   return (
-    <ul className={styles.nav}>
-      <li>Work</li>
-      <li>Contact</li>
-    </ul>
+    <header className={styles.header}>
+      <div className="container">
+        <ul className={styles.nav}>
+          <li className={styles.navItem}>
+            <SlideUpButton text="Work" classNames="isWhite" />
+          </li>
+          <li className={styles.navItem}>
+            <SlideUpButton text="Contact" classNames="isWhite" />
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 };
 
