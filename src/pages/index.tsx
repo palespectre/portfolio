e656e98fb6c1e29f6134b1e95f632b7e49dcf1a7
pierre <pierre@dahive.fr>
2022-11-06 21:24:44 +0100

    Animate sections with Locomotive-scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 
 import Nav from '@/components/Nav';
 import Hero from '@/components/Hero';
+import Works from '@/components/Works';
 
 import styles from '@/styles/Home.module.css';
 
@@ -10,30 +11,19 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>TypeScript starter for Next.js</title>
-        <meta
-          name="description"
-          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
-        />
+        <title>Pierre Cormier, portfolio</title>
+        <meta name="description" content="Hey, welcome to my portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Nav />
       <main className={styles.main}>
         <Hero />
-        <div
-          data-scroll
-          data-scroll-section
-          style={{ height: `100vh`, backgroundColor: `black` }}
-        ></div>
+        <Works />
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://vercel.com?utm_source=typescript-nextjs-starter" target="_blank" rel="noopener noreferrer">
           Powered by{` `}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
